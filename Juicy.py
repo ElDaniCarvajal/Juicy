0cf2266cd79bf035e7d8eec6d8dceb945a773aac
Franciscodc <Franciscodc_@hotmail.com>
2016-02-02 01:50:32 -0600

    Update Juicy.py

--- Juicy.py
+++ Juicy.py
@@ -14,10 +14,20 @@ alldorks=["wp-login.php","phpmyadmin","robots.txt","htaccess.txt","wp-admin/post
 "manager/media/editor/plugins/ImageManager/manager.php","gadmin/index.php","vb/install/upgrade.php","vb/install/install.php","configuration.php-dist","logs/access.log","log/access.log","access.log","index.php?option=com_jeajaxeventcalendar","jscripts/tiny_mce/plugins/tinybrowser","modules/rmgallery","modules/debaser","phpwcms/index.php?id=","modules/lykos_reviews/","webquest/soporte_derecha_w.php?","modules/articles/index.php?cat_id=","index.php?option=com_jombib",
 "modules/wfsection/","wp-content/plugins/age-verification/age-verification.php","includes/config.php","private_files","dbg-wizard.php","wp-admin/admin-ajax.php?action=revslider_ajax_action","plugins/wp-backitup/","security/xamppdirpasswd.txt","cacti/graph_view.php","cacti/graph.php?","backup","sql/backup","reports/rwservlet","administrator/index.php?autologin=1&passwd=[password]&username=[username]","data/nanoadmin.php","jenkins/login","module.php/core/loginuserpass.php",
 "wp-content/uploads/ ","wp-config.txt","secure/login.aspx","wp-content/uploads/dump.sql","root/etc/passwd"]
+
+Wordpress=[]
+Joomla=[]
+Drupal=[]
+Panel=[]
 i = 0
 
 
 parser=argparse.ArgumentParser(description="Selecciona una Opcion")
+parser.add_argument('-A','--all',help='Use All Dorks',action='store_true',default=False)
+parser.add_argument('-W','--wordpress',help='Use Wordpress Dorks',action='store_true',default=False)
+parser.add_argument('-J','--joomla', help='Use Joomla Dorks',action='store_true',default=False)
+parser.add_argument('-D','--drupal',help='Use Drupal Dorks',action='store_true',default=False)
+parser.add_argument('-P','--panel',help='Find Admin Panel',action='store_true',default=False)
 parser.add_argument('-V','--verbose',help='Muestra mas informacion',action='store_true',default=False)
 parser.add_argument('objetivo',help="Ingresa el Objetivo",type=str)
 args=parser.parse_args()
@@ -40,9 +50,5 @@ else:
 		if attack.getcode() == 200:
 			print G + "[+]" + W + " http://" + url + "/" + alldorks[i]
 			i=i+1
-	
-
-	
-	
-
-raw_input("Press Enter to Quit")
+		else:
+			i=i+1
